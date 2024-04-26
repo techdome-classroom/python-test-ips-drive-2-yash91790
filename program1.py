b3b6cac0285bedb612861eccb2484c65ecec1c63
yash91790 <146101004+yash91790@users.noreply.github.com>
2024-04-26 16:31:24 +0530

    yash verma

--- program1.py
+++ program1.py
@@ -5,6 +5,19 @@ class Solution(object):
         :rtype: bool
         """
         pass
+
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return  False
+            else:
+                stack.append(char)
+
+        return not stack    
     
 
 
--- program2.py
+++ program2.py
@@ -5,3 +5,18 @@ class Solution(object):
         :rtype: int
         """
         pass
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        i = 0
+        while i < len(s) - 1:
+            current_value = roman_dict[s[i]] 
+            next_value = roman_dict[s[i + 1]]
+            if current_value < next_value:
+                total += next_value - current_value
+                i += 1
+            else:
+                total += current_value
+            i += 1
+        if i == len(s) - 1:
+            total += roman_dict[s[i]]
+        return total
